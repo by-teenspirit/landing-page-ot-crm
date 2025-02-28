bc1aee40efa07dc3228bce7941ab324803b990ce
Mitsiky <callistalarrivee@gmail.com>
2025-02-28 15:08:18 +0100

    Amélioration de la page
    Repasse sur les bénéfices clés & les témoignages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import {
   BarChart3,
-  Clock,
+  Mails,
   Heart,
-  LineChart,
+  NotebookPen,
   MessageSquare,
   Rocket,
   Users,
-  Wallet,
+  Handshake,
+  ArrowLeftRight,
+  ClipboardPlus,
+  Star,
 } from "lucide-react";
 
 function App() {
@@ -18,7 +21,7 @@ function App() {
         <div className="container mx-auto px-6 py-4">
           <div className="flex items-center justify-between">
             <div className="flex items-center space-x-2">
-              <Rocket className="h-8 w-8 text-blue-600" />
+              <Rocket className="h-8 w-8 text-blue-600 bg-blue" />
               <span className="text-xl font-bold text-gray-900">OT-CRM</span>
             </div>
             <button className="bg-blue-600 text-white px-6 py-2 rounded-full hover:bg-blue-700 transition-colors">
@@ -37,8 +40,9 @@ function App() {
               votre Office de Tourisme
             </h1>
             <p className="text-xl text-gray-600 mb-12">
-              Centralisez vos contacts, personnalisez l'expérience visiteur et
-              collaborez efficacement avec vos partenaires touristiques.
+              Simplifiez votre gestion touristique : centralisez vos contacts,
+              organisez les séminaires, planifiez les guides et pilotez
+              adhésions, devis et campagnes marketing en toute fluidité.
             </p>
             <button className="bg-blue-600 text-white px-8 py-4 rounded-full text-lg font-semibold hover:bg-blue-700 transition-colors">
               Réserver une démo
@@ -46,7 +50,7 @@ function App() {
           </div>
           <div className="relative rounded-2xl overflow-hidden">
             <img
-              src="dist\assets\office-du-tourisme-de-bordeaux-shadow.png"
+              src="src\assets\office-du-tourisme-de-bordeaux-shadow.png"
               alt="Office de tourisme moderne"
               className="w-full h-full object-cover"
             />
@@ -99,24 +103,169 @@ function App() {
       </section>
 
       {/* Testimonial Section */}
-      <section className="py-20 bg-blue-600 text-white">
-        <div className="container mx-auto px-6">
-          <div className="max-w-3xl mx-auto text-center">
-            <img
-              className="mx-auto h-12 mb-8"
-              src="https://pro-bordeaux-tourisme.com/wp-content/uploads/2024/05/logo-bordeaux.png"
-              alt=""
-            />
-            <blockquote className="text-2xl font-medium mb-8">
-              "Grâce à ce nouveau CRM et à l'application mobile, nous avons pu
-              réduire de 40 % le temps de traitement des réservations et
-              augmenter la satisfaction visiteurs. Nos guides sont mieux
-              coordonnés, et nos campagnes marketing ont gagné en efficacité."
+      <section className="py-20 bg-blue-600 text-gray-700">
+        <div className="container grid md:grid-cols-2 gap-20 mx-auto px-6">
+          <div className="rounded-xl bg-white p-10 max-w-3xl mx-auto shadow-lg">
+            <div className="flex flex-row">
+              <Star
+                fill="gold"
+                strokeWidth={0}
+                className="h-8 w-8 text-blue-600"
+              />
+              <Star
+                fill="gold"
+                strokeWidth={0}
+                className="h-8 w-8 text-blue-600"
+              />
+              <Star
+                fill="gold"
+                strokeWidth={0}
+                className="h-8 w-8 text-blue-600"
+              />
+              <Star
+                fill="gold"
+                strokeWidth={0}
+                className="h-8 w-8 text-blue-600"
+              />
+              <Star
+                fill="gold"
+                strokeWidth={0}
+                className="h-8 w-8 text-blue-600"
+              />
+            </div>
+            <blockquote className="text-xl font-medium mb-8 pt-3">
+              "Le suivi des adhésions et des campagnes marketing est enfin
+              simplifié ! Avant, nos contacts étaient dispersés dans plusieurs
+              outils et boîtes mail, mais aujourd’hui, tout est centralisé et
+              partagé entre les services. Je peux facilement relancer les
+              membres et optimiser les renouvellements sans risquer de perdre
+              des informations précieuses."
             </blockquote>
-            <cite className="text-lg opacity-90">
-              – Office du Tourisme de Bordeaux –
-            </cite>
+            <hr className="my-6 w-full border-(--pattern-fg)" />
+            <div className="text-lg font-bold">Virginie</div>
+            <div className="opacity-90 text-sm">
+              Responsable marketing & adhésions
+            </div>
           </div>
+          <div className="flex flex-col rounded-xl bg-white p-10 max-w-3xl mx-auto shadow-lg">
+            <div className="flex flex-row">
+              <Star
+                fill="gold"
+                strokeWidth={0}
+                className="h-8 w-8 text-blue-600"
+              />
+              <Star
+                fill="gold"
+                strokeWidth={0}
+                className="h-8 w-8 text-blue-600"
+              />
+              <Star
+                fill="gold"
+                strokeWidth={0}
+                className="h-8 w-8 text-blue-600"
+              />
+              <Star
+                fill="gold"
+                strokeWidth={0}
+                className="h-8 w-8 text-blue-600"
+              />
+              <Star
+                fill="gold"
+                strokeWidth={0}
+                className="h-8 w-8 text-blue-600"
+              />
+            </div>
+            <blockquote className="text-xl font-medium mb-8 pt-3">
+              "Organiser les visites est devenu un jeu d’enfant ! Avant, on
+              préparait tout manuellement dans nos boîtes mail, ce qui était
+              source de confusion. Maintenant, tous les contacts sont récupérés
+              et partagés avec l’équipe, ce qui améliore la coordination avec
+              les guides et garantit une expérience fluide pour les visiteurs."
+            </blockquote>
+            <hr className="my-6 w-full border-(--pattern-fg)" />
+            <div className="text-lg font-bold">Émilie</div>
+            <div className="opacity-90 text-sm">Coordinatrice des visites</div>
+          </div>
+          <div className="rounded-xl bg-white p-10 max-w-3xl mx-auto shadow-lg">
+            <div className="flex flex-row">
+              <Star
+                fill="gold"
+                strokeWidth={0}
+                className="h-8 w-8 text-blue-600"
+              />
+              <Star
+                fill="gold"
+                strokeWidth={0}
+                className="h-8 w-8 text-blue-600"
+              />
+              <Star
+                fill="gold"
+                strokeWidth={0}
+                className="h-8 w-8 text-blue-600"
+              />
+              <Star
+                fill="gold"
+                strokeWidth={0}
+                className="h-8 w-8 text-blue-600"
+              />
+              <Star
+                fill="gold"
+                strokeWidth={0}
+                className="h-8 w-8 text-blue-600"
+              />
+            </div>
+            <blockquote className="text-xl font-medium mb-8 pt-3">
+              "Envoyer des devis et des brochures est tellement plus rapide
+              maintenant ! Tout est centralisé, ce qui me permet de mieux suivre
+              chaque dossier et de savoir précisément où en est chaque demande.
+              Avant, on manquait parfois d’informations et le suivi des devis
+              était compliqué, mais aujourd’hui, on a une meilleure visibilité
+              et une gestion plus efficace."
+            </blockquote>
+            <hr className="my-6 w-full border-(--pattern-fg)" />
+            <div className="text-lg font-bold">Caroline</div>
+            <div className="opacity-90 text-sm">
+              Responsable des relations clients
+            </div>
+          </div>
+          <div className="flex flex-col rounded-xl bg-white p-10 max-w-3xl mx-auto shadow-lg">
+            <div className="flex flex-row">
+              <Star
+                fill="gold"
+                strokeWidth={0}
+                className="h-8 w-8 text-blue-600"
+              />
+              <Star
+                fill="gold"
+                strokeWidth={0}
+                className="h-8 w-8 text-blue-600"
+              />
+              <Star
+                fill="gold"
+                strokeWidth={0}
+                className="h-8 w-8 text-blue-600"
+              />
+              <Star
+                fill="gold"
+                strokeWidth={0}
+                className="h-8 w-8 text-blue-600"
+              />
+              <Star
+                fill="gold"
+                strokeWidth={0}
+                className="h-8 w-8 text-blue-600"
+              />
+            </div>
+            <blockquote className="text-xl font-medium mb-8 pt-3 h-full">
+              "Grâce à l’application, je peux voir toutes mes visites en un coup
+              d'œil, gérer mon agenda et recevoir des notifications dès qu’une
+              visite est acceptée. Fini les oublis et les mauvaises surprises,
+              tout est centralisé et facile à suivre !"
+            </blockquote>
+            <hr className="my-6 w-full border-(--pattern-fg)" />
+            <div className="text-lg font-bold">Thomas</div>
+            <div className="opacity-90 text-sm">Guide touristique</div>
+          </div>{" "}
         </div>
       </section>
 
@@ -129,13 +278,13 @@ function App() {
           <div className="grid md:grid-cols-2 gap-8 max-w-4xl mx-auto">
             <div className="relative rounded-2xl overflow-hidden">
               <img
-                src="dist\assets\marketing-extra.png"
+                src="src\assets\marketing-extra.png"
                 alt="Office de tourisme moderne"
               />
             </div>{" "}
             <div className="relative rounded-2xl overflow-hidden">
               <img
-                src="dist\assets\app-soft-shadow.png"
+                src="src\assets\app-soft-shadow.png"
                 alt="Office de tourisme moderne"
                 className="w-full h-full object-cover"
               />
@@ -187,40 +336,40 @@ function App() {
 
 const benefits = [
   {
-    icon: <Clock className="h-8 w-8 text-blue-600" />,
-    title: "Gain de temps et d'efficacité",
-    description:
-      "Centralisez vos données, automatisez les tâches répétitives et réduisez le temps consacré aux opérations manuelles.",
-  },
-  {
-    icon: <Heart className="h-8 w-8 text-blue-600" />,
-    title: "Amélioration de l'expérience visiteur",
-    description:
-      "Offrez une expérience personnalisée avec un suivi fluide des demandes et une communication instantanée multilingue.",
-  },
-  {
-    icon: <Wallet className="h-8 w-8 text-blue-600" />,
-    title: "Optimisation économique",
-    description:
-      "Réduisez vos coûts de gestion et concentrez-vous sur des initiatives stratégiques à forte valeur ajoutée.",
-  },
-  {
     icon: <Users className="h-8 w-8 text-blue-600" />,
-    title: "Collaboration renforcée",
+    title: "Centralisation des contacts",
     description:
-      "Gérez efficacement vos partenariats, suivez les disponibilités et mettez en avant les acteurs locaux.",
+      "Regroupez et gérez facilement l’ensemble de vos contacts, qu’il s’agisse de clients, de partenaires ou de guides, pour un suivi optimisé et une meilleure collaboration.",
   },
   {
-    icon: <BarChart3 className="h-8 w-8 text-blue-600" />,
-    title: "Analyses avancées",
+    icon: <ArrowLeftRight className="h-8 w-8 text-blue-600" />,
+    title: "Amélioration de l'interaction avec les guides",
     description:
-      "Accédez à des statistiques détaillées sur la fréquentation, la satisfaction et le ROI de vos campagnes.",
+      "Une application dédiée pour une gestion simplifiée : offrez aux guides un outil pratique pour déclarer leurs disponibilités, gérer les visites récurrentes et suivre les approbations en temps réel, garantissant ainsi une organisation fluide.",
   },
   {
-    icon: <LineChart className="h-8 w-8 text-blue-600" />,
-    title: "Marketing intelligent",
+    icon: <Handshake className="h-8 w-8 text-blue-600" />,
+    title: "Gestion fluide des relations clients",
     description:
-      "Créez des campagnes ciblées et automatisées pour fidéliser vos visiteurs et partenaires.",
+      "Centralisez vos échanges avec vos clients grâce à des fonctionnalités dédiées aux devis, brochures et autres supports de communication, pour un service rapide et structuré.",
+  },
+  {
+    icon: <Mails className="h-8 w-8 text-blue-600" />,
+    title: "Gestion efficace des campagnes marketing",
+    description:
+      "Planifiez, suivez et analysez vos campagnes marketing pour toucher le bon public au bon moment et booster votre visibilité.",
+  },
+  {
+    icon: <ClipboardPlus className="h-8 w-8 text-blue-600" />,
+    title: "Optimisation des adhésions",
+    description:
+      "Relancez facilement les adhérents, suivez les paiements et optimisez la gestion de votre base de guides pour une meilleure rentabilité et un gain de temps considérable.",
+  },
+  {
+    icon: <NotebookPen className="h-8 w-8 text-blue-600" />,
+    title: "Organisation simplifiée des séminaires",
+    description:
+      "Gérez efficacement la logistique de vos séminaires en centralisant inscriptions, ressources et communications pour garantir un événement réussi.",
   },
 ];
 
